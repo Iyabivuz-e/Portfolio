c971970575f0b3ca78bc007d721d0641981f34c5
Dieudonne <iyabivuzed@gmail.com>
2024-04-24 12:32:14 +0530

    Modified about us content

--- src/Components/About/AboutMe.tsx
+++ src/Components/About/AboutMe.tsx
@@ -11,14 +11,14 @@ const AboutMe = () => {
         with new technologies to expand one's skill set. devoted to lifelong
         learning and skill development.
       </p>
-      <p className="leading-8 mt-3 dark:text-slate-100">
+      {/* <p className="leading-8 mt-3 dark:text-slate-100">
         I am a dedicated and passionate person with a strong passion for
         software engineering especially web development, with a focus on
         front-end technologies. Exhibits expertise in both front-end and
         back-end development, with a persistent desire to learn and experiment
         with new technologies to expand one's skill set. devoted to lifelong
         learning and skill development.
-      </p>
+      </p> */}
     </div>
   );
 }
