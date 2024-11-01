320f94eb436484060ddb317d99558135384b4d8a
Dieudonne <iyabivuzed@gmail.com>
2024-11-01 22:16:24 +0200

    updated the skills component

--- src/Components/About/Skills.tsx
+++ src/Components/About/Skills.tsx
@@ -17,37 +17,37 @@ const Skills = () => {
   };
 
   return (
-    <div>
+    <div className="">
       <h1 className="text-xl mb-5 font-bold text-base-neutral">My Skills</h1>
       <div>
-        <ul className="py-3 flex gap-3 mb-5 max-sm:flex-wrap justify-start">
+        <ul className="py-3 flex max-sm:grid grid-cols-2 bg-red-500 gap-3 mb-5 justify-start">
           <li
             onClick={() => handleFilter("All")}
-            className="bg-green-500 btn text-neutral px-4 py-2 cursor-pointer font-semibold"
+            className="bg-green-500 btn max-sm:w-full text-neutral px-4 py-2 cursor-pointer font-semibold"
           >
             All
           </li>
           <li
             onClick={() => handleFilter("Frontend")}
-            className="bg-green-500 btn text-neutral px-4 py-2 cursor-pointer font-semibold"
+            className="bg-green-500 btn max-sm:w-full text-neutral px-4 py-2 cursor-pointer font-semibold"
           >
             Frontend
           </li>
           <li
             onClick={() => handleFilter("Backend")}
-            className="bg-green-500 btn text-neutral px-4 py-2 cursor-pointer font-semibold"
+            className="bg-green-500 btn max-sm:w-full text-neutral px-4 py-2 cursor-pointer font-semibold"
           >
             Backend
           </li>
           <li
             onClick={() => handleFilter("Database")}
-            className="bg-green-500 btn text-neutral px-4 py-2 cursor-pointer font-semibold"
+            className="bg-green-500 btn max-sm:w-full text-neutral px-4 py-2 cursor-pointer font-semibold"
           >
             Database
           </li>
           <li
             onClick={() => handleFilter("Version-Control")}
-            className="bg-green-500 btn text-neutral px-4 py-2 cursor-pointer font-semibold"
+            className="bg-green-500 btn max-sm:w-full text-neutral px-4 py-2 cursor-pointer font-semibold"
           >
             Version-Control
           </li>
@@ -59,7 +59,7 @@ const Skills = () => {
             key={skill.id}
             className="text-base-neutral px-2 py-1 flex flex-col items-center justify-center"
           >
-            <button  className=" btn ">{skill.name}</button>
+            <button className=" btn ">{skill.name}</button>
           </div>
         ))}
       </div>
